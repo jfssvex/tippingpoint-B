7cd37f58ab64c3c444bff9dd1c4b4e7220ee2f90
Aritro <29025984+AritroSaha10@users.noreply.github.com>
2021-07-02 20:16:50 -0400

    setup files for autobalance

---
+++ include/macros.h
@@ -0,0 +1,7 @@
+/**
+ * \brief Balance the robot on the platform automatically.
+ * 
+ * Uses PID and the IMU to balance itself on the platform. 
+ * Example can be viewed here: https://www.youtube.com/watch?v=fT9l_AFLMho
+*/ 
+void autoBalanceRobot();
