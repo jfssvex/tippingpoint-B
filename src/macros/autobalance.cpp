e39b97f6dce40214af7aaacd880a09062fcbf875
Aritro <29025984+AritroSaha10@users.noreply.github.com>
2021-07-03 12:37:47 -0400

    tried to fix? not sure

--- src/macros/autobalance.cpp
+++ src/macros/autobalance.cpp
@@ -5,7 +5,7 @@
 
 void autoBalanceRobot() {
     // Move the drivetrain to be completely on the platform
-    driveTrainPID.moveToPoint(Vector2(2, 0));
+    driveTrainPID.moveToOrientation(Vector2(2, 0), degToRad(90));
 
     // Create a PID controller for the pitch
     PIDController pitchController = PIDController(
@@ -17,8 +17,8 @@ void autoBalanceRobot() {
 
     // Stop until its settled
     while (!pitchController.isSettled()) {
-        // Run a step of the controller with the new IMU pitch value
-        double vel = pitchController.step(imuSensor.get_pitch());
+        // Run a step of the controller with the new IMU pitch value, also scale vel to [-1, 1]
+        double vel = pitchController.step(imuSensor.get_pitch()) / 45.;
 
         // Set the motors to the new vel
         driveTrainPID.move(Vector2(vel, 0), 0);
