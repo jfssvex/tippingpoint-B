26a0955304108e38c7c5ed5702dcd7bcc5413a70
Aritro <29025984+AritroSaha10@users.noreply.github.com>
2021-07-08 12:03:00 -0400

    added basic opcontrol

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -1,5 +1,12 @@
 #include "main.h"
+#include "globals.h"
 
 void myOpControl() {
-    // TODO: Add basic opcontrol
+    // Basic op control using tank drive
+    while (true) {
+        int power = masterController.get_analog(ANALOG_LEFT_Y);
+        int turn = masterController.get_analog(ANALOG_RIGHT_X);
+
+        driveTrain->arcade(power, turn, 0.5); // TODO: Change threshold to something useful
+    }    
 }
