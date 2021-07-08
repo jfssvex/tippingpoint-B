b7403b385e8404ea8629a6edc3cb2cc3a94928d7
Aritro <29025984+AritroSaha10@users.noreply.github.com>
2021-07-08 12:19:27 -0400

    added cubic scale to joystick

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -2,11 +2,21 @@
 #include "globals.h"
 
 void myOpControl() {
-    // Basic op control using tank drive
+    // Basic op control using arcade drive
     while (true) {
         int power = masterController.get_analog(ANALOG_LEFT_Y);
         int turn = masterController.get_analog(ANALOG_RIGHT_X);
 
-        driveTrain->arcade(power, turn, 0.5); // TODO: Change threshold to something useful
+        driveTrain->arcade(joystickCubicDrive(power), joystickCubicDrive(turn), 0.5); // TODO: Change threshold to something useful
     }    
+}
+
+/**
+ * \brief Scale joystick output to cubic graph
+ * 
+ * Scales raw value in range [-127, 127] to range [-1, 1] and apply exponent 3 in order to allow for more driver accuracy
+*/
+double joystickCubicDrive(int raw) {
+    double scaledRaw = ((double)raw) / 127.0;
+    return pow(scaledRaw, 3) * 127;
 }
