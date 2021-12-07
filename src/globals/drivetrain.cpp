e996fe9b07138006ebed1eee6e4e7a3a28234408
Aritro <29025984+AritroSaha10@users.noreply.github.com>
2021-12-07 16:16:02 -0500

    fix: changed pid constants

--- src/globals/drivetrain.cpp
+++ src/globals/drivetrain.cpp
@@ -3,7 +3,7 @@
 
 // PID Gain Constants
 PIDInfo driveConstants(1, 1, 1);
-PIDInfo turnConstants(0, 0, 0);
+PIDInfo turnConstants(1, 0, 0);
 
 // Definitions
 SkidSteerDrive* driveTrain = new SkidSteerDrive(&tLeft, &tRight, &bLeft, &bRight);
