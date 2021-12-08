7d43020ec7379769aab357746d4e309f053ecc73
Aritro Saha <29025984+AritroSaha10@users.noreply.github.com>
2021-12-08 10:35:05 -0500

    fix: changed pid consts + tolerances to last years

--- src/globals/drivetrain.cpp
+++ src/globals/drivetrain.cpp
@@ -2,9 +2,17 @@
 #include "chassis.h"
 
 // PID Gain Constants
-PIDInfo driveConstants(1, 1, 1);
-PIDInfo turnConstants(1, 1, 1);
+PIDInfo driveConstants(0.2, 0.000001, 0.1);
+PIDInfo turnConstants(1.5, 0.1, 0.5);
 
 // Definitions
 SkidSteerDrive* driveTrain = new SkidSteerDrive(&tLeft, &tRight, &bLeft, &bRight);
-DrivetrainPID driveTrainPID(driveTrain, driveConstants, turnConstants, 0, 0);
+DrivetrainPID driveTrainPID(
+    driveTrain, 
+    driveConstants, 
+    turnConstants, 
+    0.7,  // Inches
+    3,    // Inches
+    0.04, // Radians
+    0.3   // Radians
+);
