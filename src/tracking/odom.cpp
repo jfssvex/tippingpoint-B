c119c3759be96eccd105c2a4d0a88bfabbba3d00
Aritro <29025984+AritroSaha10@users.noreply.github.com>
2021-12-15 18:04:26 -0500

    fix: changed odom again

--- src/tracking/odom.cpp
+++ src/tracking/odom.cpp
@@ -20,99 +20,120 @@ float lLast = 0; // Last value of left tracking wheel
 float rLast = 0; // Last value of right tracking wheel
 float bLast = 0; // Last value of back tracking wheel
 
-float  halfA = 0;
-
 // Constants and macros
 const float lrOffset = WHEELBASE / 2.0f; // Offset of the left / right tracking wheel from the center in terms of x axis
 // const float bOffset = -BACK_WHEEL_OFFSET; // Offset of the back tracking wheel from the center in terms of y axis (negative because its in the back)
 const float bOffset = 0; // Offset of the back tracking wheel, 0 because there is none
-
-bool printTracking = true;
 
 // Conversion calculations
 #define DRIVE_DEGREE_TO_INCH (M_PI * DRIVE_WHEEL_DIAMETER / 360) 
 #define TRACKING_WHEEL_DEGREE_TO_INCH (M_PI * TRACKING_WHEEL_DIAMETER / 360)
 
 // Actual tracking function that runs in BG
-void tracking(void* param) {
+void tracking(void* parameter) {
+    // Assuming that there are 3 encoders
+    // Also, I don't know if this works
 
-	// Initialize variables
-	lLast = 0;
-	rLast = 0;
-	bLast = 0;
-	float x = trackingData.getPos().getX();
-	float y = trackingData.getPos().getY();
-	float left = 0;
-	float right = 0;
-	float lateral = 0;
-	float angle = 0;
+    // Initialize variables
+    lLast = 0; // Last encoder value of left
+    rLast = 0; // Last encoder value of right
+    bLast = 0; // Last encoder value of back
 
-	uint32_t printTime = pros::millis();
+    Vector2 globalPos(0, 0);
 
-	// Start tracking loop
-	while(1) {
-		float localX, localY = 0;
+    float left = 0; // Total distance travelled by left tracking wheel
+    float right = 0; // Total distance travelled by right tracking wheel
+    float lateral = 0; // Total distance travelled laterally (measured from back tracking wheel)
+    float angle = 0; // Current arc angle
 
-		// Get encoder data
-		float leftEncoder = bLeft.get_position();
-		float rightEncoder = 0;
-		float backEncoder = 0;
+    uint32_t printTime = pros::millis();
 
-		// Calculate deltas
-		lDelta = leftEncoder - lLast;
-		rDelta = rightEncoder - rLast;
-		bDelta = backEncoder - bLast;
-		lDist = lDelta * DRIVE_DEGREE_TO_INCH;
-		rDist = rDelta * DRIVE_DEGREE_TO_INCH;
-		bDist = bDelta * DRIVE_DEGREE_TO_INCH;
+    // Reset all motor positions
+    for (pros::Motor* tmp : driveTrain->allMotors) {
+        tmp->tare_position();
+    }
 
-		// Store readings for next deltas
-		lLast = leftEncoder;
-		rLast =	rightEncoder;
-		bLast = backEncoder;
+    // Reset encoders to 0 before starting
+    // lEnc.reset();
+    // rEnc.reset();
+    // bEnc.reset();
 
-		// Increment persistent variables
-		left += lDist;
-		right += rDist;
-		lateral += bDist;
+    // Tracking loop
+    while (true) {
+        Vector2 localPos;
 
-		// Calculate arc angle
-		float holdAngle = angle;
-		angle = (right - left)/(lrOffset * 2.0f);
-		aDelta = angle - holdAngle;
+        // Get encoder data, directly fron wheels because no tracking wheels yet
+        float lEncVal = bLeft.get_position();
+        float rEncVal = 0;
+        float bEncVal = 0;
 
-		// If theres an arc
-		if(aDelta) {
-			localY = ((rDist / aDelta - lrOffset) * sin(aDelta/2.0f)) * 2.0f;
+        // Calculate delta values
+        lDelta = lEncVal - lLast;
+        rDelta = rEncVal - rLast;
+        bDelta = bEncVal - bLast;
 
-			localX = ((bDist / aDelta - BACK_WHEEL_OFFSET) * sin(aDelta/2.0f)) * 2.0f;
-		}
-		// If no arc
-		else {
-			halfA = 0;
-			aDelta = 0;
-			localY = (rDist+lDist)/2;
-			localX = bDist;
-		}
+        // Calculate IRL distances from deltas
+        lDist = lDelta * DRIVE_DEGREE_TO_INCH;
+        rDist = rDelta * DRIVE_DEGREE_TO_INCH;
+        bDist = bDelta * DRIVE_DEGREE_TO_INCH;
 
-		float p = -(halfA + holdAngle); // The global ending angle of the robot
-		float cosP = cos(p);
-		float sinP = sin(p);
+        // Update last values for next iter since we don't need to use last values for this iteration
+        lLast = lEncVal;
+        rLast = rEncVal;
+        bLast = bEncVal;
 
-		// Update the global position
-		float dY = (localY * cosP) - (localX * sinP);
-		float dX = (localY * sinP) + (localX * cosP);
+        // Update total distance vars
+        left += lDist;
+        right += rDist;
+        lateral += bDist;
 
-		trackingData.update(trackingData.getPos().getX() + dX, trackingData.getPos().getY() +dY, trackingData.getHeading() + aDelta);
+        // Calculate new absolute orientation
+        float prevAngle = angle; // Previous angle, used for delta
+        angle = (right - left) / (lrOffset * 2.0f);
 
-		// Print debug
-		if(pros::millis() - printTime > 50) {
-			printf("X: %f, Y: %f, A: %f\n", trackingData.getPos().getX(), trackingData.getPos().getY(), radToDeg(trackingData.getHeading()));
-			printTime = pros::millis();
-			// printf("X: %f, Y: %f", left, right);
-			// printf("A: %f", lateral);
-		}
+        // Get angle delta
+        aDelta = angle - prevAngle;
 
-		pros::delay(5);
-	}
+        // Calculate using different formulas based on if orientation change
+        float avgLRDelta = (lDist + rDist) / 2; // Average of delta distance travelled by left and right wheels
+        if (aDelta == 0.0f) {
+            // Set the local positions to the distances travelled since the angle didn't change
+            localPos = Vector2(bDist, avgLRDelta);
+        } else {
+            // Use the angle to calculate the local position since angle did change
+            localPos = Vector2(
+                2 * sin(aDelta / 2) * (bDist / aDelta - bOffset),
+                2 * sin(aDelta / 2) * (avgLRDelta / aDelta - lrOffset)
+            );
+        }
+
+        // Calculate the average orientation
+        float avgAngle = -(prevAngle + (aDelta / 2));
+
+        // Calculate global offset https://www.mathsisfun.com/polar-cartesian-coordinates.html
+        float globalOffsetX = cos(avgAngle); // cos(θ) = x 
+        float globalOffsetY = sin(avgAngle); // sin(θ) = y 
+
+        // Finally, update the global position
+        globalPos = Vector2(
+            globalPos.getX() + (localPos.getY() * globalOffsetY) + (localPos.getX() * globalOffsetX),
+            globalPos.getY() + (localPos.getY() * globalOffsetX) - (localPos.getX() * globalOffsetY)
+        );
+
+        // Update tracking data
+        // trackingData.update(globalPos.getX(), globalPos.getY(), degToRad(myImu.get_rotation()));
+        trackingData.update(globalPos, trackingData.getHeading() + aDelta);
+
+        // Debug print
+        if (pros::millis() - printTime > 75) {
+            // Only print every 75ms to reduce lag
+            printf("X: %f, Y: %f, A: %f\n", 
+                    trackingData.getPos().getX(), 
+                    trackingData.getPos().getY(), 
+                    radToDeg(trackingData.getHeading()));
+            printTime = pros::millis();
+        }
+        
+        pros::delay(5); // Max of 10ms
+    }
 }
