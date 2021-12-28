1ede9524876d46f63f6ab8a6a24c6ecd20fca1a6
AritroSaha10 <AritroSaha10@users.noreply.github.com>
2021-12-28 02:37:53 +0000

    deploy: 0bdab1fb7f4cd412b2b0f220ce73234050ec620e

--- class_drivetrain_p_i_d.js
+++ class_drivetrain_p_i_d.js
@@ -4,7 +4,7 @@ var class_drivetrain_p_i_d =
     [ "~DrivetrainPID", "class_drivetrain_p_i_d.html#aa082af05a9e58ac7519e0351cc70f441", null ],
     [ "getDriveController", "class_drivetrain_p_i_d.html#a7473ac8518fe003e0f05ca11380502f3", null ],
     [ "getTurnController", "class_drivetrain_p_i_d.html#a0f6155f37a0ef28ceb1f471646663378", null ],
-    [ "move", "class_drivetrain_p_i_d.html#a1cfd65ef0ba6c56fc67db76ffaa40081", null ],
+    [ "move", "class_drivetrain_p_i_d.html#a75b7aef8cec9054d0c227f32bd255377", null ],
     [ "moveRelative", "class_drivetrain_p_i_d.html#a074fc014ef92c0796d09fcc65a062379", null ],
     [ "moveToOrientation", "class_drivetrain_p_i_d.html#ada2c1e15f842228323f177d2111d6ed1", null ],
     [ "moveToPoint", "class_drivetrain_p_i_d.html#ad5d10e3e16091c47bbfa7dd1a4c96d8f", null ],
--- search/all_9.js
+++ search/all_9.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['main_2eh_67',['main.h',['../main_8h.html',1,'']]],
-  ['move_68',['move',['../class_drivetrain_p_i_d.html#a1cfd65ef0ba6c56fc67db76ffaa40081',1,'DrivetrainPID']]],
+  ['move_68',['move',['../class_drivetrain_p_i_d.html#a75b7aef8cec9054d0c227f32bd255377',1,'DrivetrainPID']]],
   ['moverelative_69',['moveRelative',['../class_drivetrain_p_i_d.html#a074fc014ef92c0796d09fcc65a062379',1,'DrivetrainPID']]],
   ['movetoorientation_70',['moveToOrientation',['../class_drivetrain_p_i_d.html#ada2c1e15f842228323f177d2111d6ed1',1,'DrivetrainPID']]],
   ['movetopoint_71',['moveToPoint',['../class_drivetrain_p_i_d.html#ad5d10e3e16091c47bbfa7dd1a4c96d8f',1,'DrivetrainPID']]],
