1029ea5ea744d18ae5f79818108983af86b14645
Aritro Saha <29025984+AritroSaha10@users.noreply.github.com>
2021-12-28 12:50:14 -0500

    fix: working on auton

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -2,26 +2,20 @@
 #include "globals.h"
 #include "macros.h"
 
-void myAuton() {
-    trackingData.update({0, 0}, 0);
-    // trackingData.update({0, 20}, 0);
-    chassis->setState({0_in, 0_in, 90_deg});
-
-    forklift1.enable();
-    forklift2.enable();
-    intake.enable();
-
+void grabMogo() {
     forklift1.goMiddle();
     forklift2.goDown();
-
-    driveTrainPID.moveToPoint({ 0, 12 });
 
     // Wait for it to reach okay tolerance levels
     do {
         pros::delay(10);
     } while (abs(forklift2.getError()) > 40);
 
+    driveTrainPID.moveToPoint({ 0, 24 });
+
     forklift2.goMiddle();
+
+    pros::delay(250);
 
     // Wait for it to reach okay tolerance levels
     do {
@@ -30,13 +24,14 @@ void myAuton() {
 
     forklift2.goUp();
 
-    driveTrainPID.moveToPoint(Vector2( 0, 0 ), true);
+    driveTrainPID.moveToPoint(Vector2( 0, 10 ), true);
 
     // Wait for it to reach okay tolerance levels
     do {
         pros::delay(10);
     } while (abs(forklift2.getError()) > 40);
 
+    /*
     startIntakeSmoothMove(false, false);
     intake.counterClockwise(80);
 
@@ -44,4 +39,34 @@ void myAuton() {
 
     driveTrainPID.move({}, 0);
     intake.stop();
+    */
+}
+
+void testForklifts() {
+    forklift2.goDown();
+
+    // Wait for it to reach okay tolerance levels
+    do {
+        pros::delay(10);
+    } while (abs(forklift2.getError()) > 40);
+
+
+    forklift2.goUp();
+
+    // Wait for it to reach okay tolerance levels
+    do {
+        pros::delay(10);
+    } while (abs(forklift2.getError()) > 40);
+}
+
+void myAuton() {
+    trackingData.update({0, 10}, 0);
+    // trackingData.update({0, 20}, 0);
+    chassis->setState({0_in, 0_in, 90_deg});
+
+    forklift1.enable();
+    forklift2.enable();
+    intake.enable();
+
+    testForklifts();
 }
